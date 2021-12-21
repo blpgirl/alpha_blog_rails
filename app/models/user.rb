04abc4fff2aa2b7295dc87093ac1b74304801b32
Leyla Bonilla <leyla.bonilla@bairesdev.com>
2021-12-21 10:18:16 -0500

    bcrypt for user passwords and user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
+  # The case sensitive false is so that it doesn't recognize as different uppercase vs normal
+  validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {minimum: 3, maximum: 25}
+  # To test the expression go to https://rubular.com/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: {maximum: 125}, format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_194327) do
+ActiveRecord::Schema.define(version: 2021_12_21_130815) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_194327) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
