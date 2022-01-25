a49df729952665d9039df602674b6ac41926fe7e
Leyla Bonilla <leyla.bonilla@bairesdev.com>
2022-01-25 16:11:47 -0500

    added a validation for the color field

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: {maximum: 125}, format: { with: VALID_EMAIL_REGEX }
 
+  # To validate that is a rgb valid color ... added /\A at the beginning expression
+  # and added \z/i at the end of the expression to make it valid for rails
+  VALID_HEX_COLOR = /\A^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$\z/i
+  validates :color, length: {minimum: 4, maximum: 7}, format: { with: VALID_HEX_COLOR }
+
   has_secure_password
 end
